b5fc751ec81a05d598ad03bb111da2ff91b5035b
wouchan <adrian.kafel@proton.me>
2025-12-29 20:54:36 +0100

    camera movement completed

---
+++ src/Assignments/CameraMovement/camera_controller.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "camera.h"
+
+class CameraControler {
+public:
+    CameraControler():camera_(nullptr), scale_(0.01f), LMB_pressed_(false) {}
+    CameraControler(Camera* camera):camera_(camera), scale_(0.01f), LMB_pressed_(false) {}
+    void set_camera(Camera *camera) { camera_ = camera; }
+
+    void rotate_camera(float dx, float dy) {
+        camera_->rotate_around_center(-scale_ * dy, camera_->x());
+        camera_->rotate_around_center(-scale_ * dx, camera_->y());
+    }
+
+    void mouse_moved(float x, float y) {
+        if (LMB_pressed_) {
+            auto dx = x - x_;
+            auto dy = y - y_;
+            x_ = x;
+            y_ = y;
+
+            rotate_camera(dx, dy);
+        }
+    };
+
+    void LMB_pressed(float x, float y) {
+        LMB_pressed_ = true;
+        x_ = x;
+        y_ = y;
+    };
+
+    void LMB_released(float x, float y) {
+        LMB_pressed_ = false;
+        auto dx = x - x_;
+        auto dy = y - y_;
+
+        rotate_camera(dx, dy);
+    };
+
+private:
+    Camera *camera_;
+    float scale_;
+    bool LMB_pressed_;
+    float x_;
+    float y_;
+};
